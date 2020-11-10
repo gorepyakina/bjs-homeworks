3e28229a91561cfda476e0d7dbe015fd4c0fca67
Anna Gorepyakina <annagorepyakina@gmail.com>
2020-11-10 11:15:03 +0300

    First commit

--- 1.1-variables/task.js
+++ 1.1-variables/task.js
@@ -7,21 +7,38 @@
 
 function averageMark(a, g, p) {
 	
-    // реализуйте здесь задание №1
+	// реализуйте здесь задание №1
+	
+	let algebra = a;
+	let geography = g;
+	let physics = p;
+	let averageValue;
+
+	averageValue = (a + g + p) / 3;
     
-	return // ваш ответ;
+	return averageValue;
 }
 
 function sayHello(userName) {
 	
-    // реализуйте здесь задание №2
+	// реализуйте здесь задание №2
+	
+	let myName = userName;
+	let message = "Привет, мир! Меня зовут ";
     
-	return // ваш ответ;
+	return message + myName;
 }
 
 function calculateFormula() {
     
 	// реализуйте здесь задание №3
+
+	let x = 2;
+	let y = 22;
+	let z = 0;
+	let rezult;
+
+	rezult = x * y + 5 * z + x - 1;
     
-    return // ваш ответ;
+    return rezult;
 }
