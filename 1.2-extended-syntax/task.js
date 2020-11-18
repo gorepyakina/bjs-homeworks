10b4ed8be1122d880d824d9bc9cfe5bb3b10d0f5
Anna Gorepyakina <annagorepyakina@gmail.com>
2020-11-18 13:32:37 +0300

    Homework JS v.2

--- 1.2-extended-syntax/task.js
+++ 1.2-extended-syntax/task.js
@@ -2,57 +2,45 @@
 function getResult(a,b,c){
     // код для задачи №1 писать здесь
     let D = b**2 - 4 * a * c;
-    let formula1 = (- b + Math.sqrt(D)) / 2 * a;
-    let formula2 = (- b - Math.sqrt(D)) / 2 * a;
-    let rezult;
-    console.log(D);
+    let resultHere = [];
 
-    if (D < 0) 
-        rezult = [];
-    else if (D === 0)
-        rezult = [formula1];
+    if (D === 0)
+        resultHere = [((- b + Math.sqrt(D)) / 2 * a)];
     else if (D > 0) 
-        rezult = [formula1, formula2];
+        resultHere = [((- b + Math.sqrt(D)) / 2 * a), ((- b - Math.sqrt(D)) / 2 * a)];
 
-    return rezult;
+    return resultHere;
     // return x;
 }
 
 function getAverageMark(marks){
     // код для задачи №2 писать здесь
-    let rezult;
     let sum = 0;
 
-    if (marks.length === 0) {
-        rezult = 0;
-    } else if (marks.length > 5) {
+    if (marks.length > 5) {
         console.log("Оценок больше 5");
-        marks.splice(5);
+        marks.splice(5);  
+    }
+   
+    if (marks.length === 0) {
+        return 0;
+    } else if (marks.length !== 0) {
         for (let i = 0; i < marks.length; i++) {
-            sum = sum + parseInt(marks[i]);
+        sum = sum + parseInt(marks[i]);
         }
-        rezult = (sum / marks.length);
-    } else if (marks.length <= 5) {
-        for (let i = 0; i < marks.length; i++) {
-            sum = sum + parseInt(marks[i]);
-        }
-        rezult = (sum / marks.length);
+        return sum / marks.length;
     }
     // return averageMark;
-    return rezult;
 }
 
 function askDrink(name,dateOfBirthday){
     // код для задачи №3 писать здесь
-    const year = new Date().getFullYear();
-    let age = (year - dateOfBirthday.getFullYear());
-    let result;
+    let age = (new Date().getFullYear() - dateOfBirthday.getFullYear());
 
     if (age > 18) {
-        result = `Не желаете ли олд-фэшн, ${name}?`;
-    } else if (age < 18) {
-        result = `Сожалею, ${name}, но я не могу вам продать алкоголь. Зато могу предложить вам замечательный клюквенный компот!`;
+        return `Не желаете ли олд-фэшн, ${name}?`;
+    } else {
+        return `Сожалею, ${name}, но я не могу вам продать алкоголь. Зато могу предложить вам замечательный клюквенный компот!`;
     }
     // return result;
-    return result;
 }
